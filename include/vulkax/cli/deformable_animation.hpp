fdfad05f47f6edb35b43aa00e8f31355a43caad6
SWAYAM SINGAL <swayam.singal@gmail.com>
2026-08-20 15:55:41 +0530

    Declare nonlinear deformable animation CLI

---
+++ include/vulkax/cli/deformable_animation.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace vulkax::cli {
+
+// Returns -1 when argv does not name this command; otherwise returns an exit code.
+int deformableAnimationCommand(int argc, char** argv);
+
+} // namespace vulkax::cli
