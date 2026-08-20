2b22cae29a86ab79e5bcd164cef398fb4397883c
SWAYAM SINGAL <swayam.singal@gmail.com>
2026-08-20 12:48:23 +0530

    Remove WorldIR entity lookup shadow warning

--- src/world/world_ir.cpp
+++ src/world/world_ir.cpp
@@ -2,16 +2,16 @@
 
 namespace vulkax::world {
 
-Entity* WorldIR::findEntity(EntityId id) noexcept {
+Entity* WorldIR::findEntity(EntityId entityId) noexcept {
     for (auto& entity : entities) {
-        if (entity.id == id) return &entity;
+        if (entity.id == entityId) return &entity;
     }
     return nullptr;
 }
 
-const Entity* WorldIR::findEntity(EntityId id) const noexcept {
+const Entity* WorldIR::findEntity(EntityId entityId) const noexcept {
     for (const auto& entity : entities) {
-        if (entity.id == id) return &entity;
+        if (entity.id == entityId) return &entity;
     }
     return nullptr;
 }
