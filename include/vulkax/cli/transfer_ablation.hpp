77067f456448039307c89677c7e9e2ea5cee8fdb
SWAYAM SINGAL <swayam.singal@gmail.com>
2026-08-20 19:24:26 +0530

    Expose transfer diagnostics CLI

--- include/vulkax/cli/transfer_ablation.hpp
+++ include/vulkax/cli/transfer_ablation.hpp
@@ -4,5 +4,6 @@ namespace vulkax::cli {
 
 // Returns -1 when argv does not name this command; otherwise returns an exit code.
 int transferAblationCommand(int argc, char** argv);
+int transferDiagnosticsCommand(int argc, char** argv);
 
 } // namespace vulkax::cli
