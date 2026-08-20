a621e4b63c69f6228ba8f3d4e1235c76044ddbc9
SWAYAM SINGAL <swayam.singal@gmail.com>
2026-08-20 14:43:24 +0530

    Add deformable reference CLI contract

---
+++ include/vulkax/cli/deformable_reference.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace vulkax::cli {
+
+// Returns -1 when argv does not name this command; otherwise returns an exit code.
+int deformableReferenceCommand(int argc, char** argv);
+
+} // namespace vulkax::cli
