176ca19aab69c5b731642142876cb064c99bbac8
SWAYAM SINGAL <swayam.singal@gmail.com>
2026-08-26 04:47:29 +0530

    Add captured observation robustness CLI interface

---
+++ include/vulkax/cli/captured_observation_robustness.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace vulkax::cli {
+
+// Returns -1 when argv does not request the captured observation robustness command.
+int capturedObservationRobustnessCommand(int argc, char** argv);
+
+} // namespace vulkax::cli
